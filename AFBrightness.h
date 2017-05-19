89e249efb0968fbf85af343082a55aa6769ba8ce
AndyPriest <1659826698@qq.com>
2017-05-19 10:39:28 +0800

    Brightness

---
+++ AFBrightness.h
@@ -0,0 +1,15 @@
+//
+//  AFBrightness.h
+//  AFBrightness
+//
+//  Created by yzl on 2017/5/19.
+//  Copyright © 2017年 yzl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AFBrightness : UIView
+//亮度调整
++ (instancetype)brightness;
+
+@end
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AFBrightness
 iOS Adjust the brightness
+导入头文件。#import "AFBrightness.h"   
+调用   [AFBrightness brightness];   即可 系统会自己监听调整亮度方法
+
